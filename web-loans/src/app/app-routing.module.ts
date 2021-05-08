27463de0cf8b6422497f9e89326523060496a4b0
Christian Rodriguez <cristian010193@gmail.com>
2021-05-08 16:51:35 -0500

    Avances pantalla login

--- web-loans/src/app/app-routing.module.ts
+++ web-loans/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { LoansComponent } from './components/principal/loans/loans.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'loans', component: LoansComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { path: '**', pathMatch: 'full', redirectTo: 'login' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- web-loans/src/app/app.module.ts
+++ web-loans/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoansComponent } from './components/principal/loans/loans.component';
+
+// Services
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    LoansComponent
   ],
   imports: [
     BrowserModule,
